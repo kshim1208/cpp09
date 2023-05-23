036422d5c8d27b1e7930c4fbb9f8e20eb82af22d
kshim1208 <a41533503@gmail.com>
2023-05-23 17:53:55 +0900

    multimap studying

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -1,6 +1,7 @@
 #include "BitcoinExchange.hpp"
 
 #include <map>
+#include <fstream>
 
 BitcoinExchange::BitcoinExchange(/* args*/){}
 
@@ -12,7 +13,16 @@ BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange& source){}
 
 void	BitcoinExchange::openMarket(void)
 {
+	std::fstream	data;
+
+	data.open("data.csv", /* 읽기 위한 옵션 추가 */);
+	if (data.is_open() == false)
+	{
+		return ;
+	}
 	// data.csv 파일을 연다.
-	// key-value 형태로 가공하여 순차적으로 저장한다.
-		// key는 int 형태로 저장한다. value는 float 형태로 저장한다.
+	// 한 줄을 읽는다.
+	// 가장 앞 숫자열을 년도 단위가 되도록 읽어낸다.
+	// 날짜 범위는 어떻게 잡을까? 제한이 있나? -> 데이터 베이스의 날짜가 int 범위를 초과하진 않음.
+	// 날짜 parsing하고 나면 이에 해당하는 key 값을 float 읽기로 저장한다.
 }
